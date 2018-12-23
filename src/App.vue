03d107edb096394050e990c65f7423bebaff68c3
Fan Ding <zichuange@gmail.com>
2018-12-23 13:58:06 +0800

    🎉 It's working!

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <Repeator />
+  </div>
+</template>
+
+<script>
+import Repeator from './components/Repeator'
+
+export default {
+  name: 'app',
+  components: {
+    Repeator,
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Repeator.vue
@@ -0,0 +1,59 @@
+<template>
+  <div>
+    <div>
+      <div>
+        <div v-for="(message, index) in messages" :key="index">
+          {{ message }}
+        </div>
+      </div>
+    </div>
+    <div>
+
+    <input
+      type="text"
+      v-model="mainInput"
+      @keyup.enter="submit"
+    >
+    </div>
+    <div>
+
+    </div>
+  </div>
+
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      mainInput: '',
+      messages: [],
+    }
+  },
+  methods: {
+    submit() {
+      this.messages.push(this.mainInput)
+      this.messages.push(this.reply(this.mainInput))
+      this.mainInput = ''
+    },
+    reply(sentence) {
+      let result = sentence
+      if ((/吗？?/).test(sentence)) {
+        result = result.replace(/吗？?/, '!')
+      }
+      if((/我/).test(sentence)) {
+        result = result.replace(/我/, '你')
+      }
+      if((/范鼎/).test(sentence)) {
+        result = result.replace(/范鼎/, '高楚钢')
+      }
+      
+
+      return result
+    }
+  },
+}
+</script>
+
+<style scoped>
+</style>
